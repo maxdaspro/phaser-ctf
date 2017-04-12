de44d215ebf17df2d2c02916b54f35fba2d15f30
bernard <janss.bernard@gmail.com>
2017-04-12 09:41:51 +0200

    rien

--- script/PlayState.js
+++ script/PlayState.js
@@ -7,11 +7,13 @@ let car;
 let enemies;
 let flag;
 let layers = {};
+let seaGroup;
 
 PlayState.preload = function(){
     game.load.tilemap("map", "assets/images/mapfinal.json", null, Phaser.Tilemap.TILED_JSON);
     game.load.image("car", "assets/images/car.png");
     game.load.image("tiles", "assets/images/tiles.png");
+    game.load.image("point", "assets/images/point.png");
     game.load.image("flag", "assets/images/flag2.png");
 }
 
@@ -20,17 +22,14 @@ PlayState.create = function(){
     map = game.add.tilemap('map');
     map.addTilesetImage('tiles');
 
-    let objs = game.add.group();
-    objs.enableBody = true;
-
     layers = {
         fond: map.createLayer('fond'),
-        bases: map.createLayer('bases'),
-        obstacle: map.createLayer('obstacle'),
-        collisions: map.createLayer('collisions'),
-        mort: map.createLayer('mort'),
+        boue: map.createLayer('boue'),
         eau: map.createLayer('eau'),
-        boue: map.createLayer('boue')
+        bases: map.createLayer('bases'),
+        mort: map.createLayer('mort'),
+        obstacle: map.createLayer('obstacle'),
+        collisions: map.createLayer('collisions')
     };
 
     layers.fond.resizeWorld();
@@ -43,12 +42,28 @@ PlayState.create = function(){
     layers.collisions.alpha = 0;
     layers.mort.alpha = 1;
 
+
+
+    /*************/
+    seaGroup = this.game.add.group();
+    seaGroup.enableBody = true;
+    seaGroup.alpha = 0.2;
+    var item;
+    let result = this.findObjectsByType('item', map, 'objectsLayer');
+    result.forEach(function(element){
+        this.createFromTiledObject(element, seaGroup);
+    }, this);
+    
+    console.log(seaGroup);
+    /**************/
+
+
     game.physics.arcade.enable(layers.mort);
 
     car = game.add.sprite(50, 290, 'car');
 
-
     game.physics.arcade.enable(car);
+
     car.scale.setTo(0.3);
     car.anchor.set(0.5);
     car.radius = car.height;
@@ -81,6 +96,7 @@ PlayState.carKill = function(){
     console.log("mort")
 }
 PlayState.carWater = function(){
+    console.log('ralenti');
     car.body.maxVelocity.set(50);
 }
 PlayState.update = function() {
@@ -88,7 +104,7 @@ PlayState.update = function() {
     game.physics.arcade.collide(car, layers.collisions);
     game.physics.arcade.overlap(car, flag, this.carFlag);
     game.physics.arcade.collide(car, layers.mort, this.carKill);
-    game.physics.arcade.overlap(car, layers.eau, this.carWater);
+    game.physics.arcade.overlap(car, seaGroup, this.carWater);
 
 
     if (cursors.up.isDown) {
@@ -115,3 +131,28 @@ PlayState.render = function() {
     // game.debug.body(car);
     // game.debug.cameraInfo(game.camera, 32, 32);
 }
+
+//find objects in a Tiled layer that containt a property called "type" equal to a certain value
+PlayState.findObjectsByType = function(type, map, layer) {
+    var result = new Array();
+    map.objects[layer].forEach(function(element){
+        if(element.type === type) {
+            //Phaser uses top left, Tiled bottom left so we have to adjust
+            //also keep in mind that the cup images are a bit smaller than the tile which is 16x16
+            //so they might not be placed in the exact position as in Tiled
+            element.y -= map.tileHeight;
+            result.push(element);
+        }
+    });
+    return result;
+};
+
+//create a sprite from an object
+PlayState.createFromTiledObject = function(element, group) {
+    var sprite = group.create(element.x, element.y, 'point');
+
+    //copy all properties to the sprite
+    Object.keys(element).forEach(function(key){
+        sprite[key] = element[key];
+    });
+};
